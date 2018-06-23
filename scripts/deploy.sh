943594f296ef568761e98d0ef39e374d0e0f9081
Gonzalo Soriano <Gonzalo@Gonzalos-MacBook-Pro.local>
2018-06-23 12:02:45 +0200

    change deploy to use dist html and src 404

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -22,7 +22,8 @@ git clone --depth 1 --branch gh-pages $GIT_REMOTE_URL deploy
 rsync -r assets/ deploy/assets/
 mkdir -p deploy/dist/
 cp dist/dauntless-builder.js deploy/dist/
-cp src/*.html deploy/
+cp dist/*.html deploy/
+cp src/404.html deploy/
 cp CNAME deploy/
 
 ### go into deploy and actually deploy
