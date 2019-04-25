bdfaf8f7292b7f314c205dec2cc7b07d4ebe4689
Christopher Kaster <me@atomicptr.de>
2019-04-26 00:49:43 +0200

    fix bug that caused specials slots to not appear

--- src/routes/BuildRoute.jsx
+++ src/routes/BuildRoute.jsx
@@ -295,7 +295,7 @@ export default class BuildRoute extends React.Component {
 
         let parts = [];
 
-        if (weaponHasParts("specials") && weapon.restrict_specials === false) {
+        if (weaponHasParts("specials") && weapon.restrict_specials !== true) {
             let slot = "weapon_part1_name";
 
             if (ItemUtility.isRepeater(weapon)) {
