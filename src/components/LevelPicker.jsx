5ff9f112e0765652c9965bd9e080a1660d63ac2a
Christopher Kaster <me@atomicptr.de>
2020-12-04 16:09:49 +0100

    fix power surged button not having a proper default state

--- src/components/LevelPicker.jsx
+++ src/components/LevelPicker.jsx
@@ -28,7 +28,7 @@ export default class LevelPicker extends React.Component {
             return <React.Fragment>
                 <i className="far fa-star"/>
                 &nbsp;
-                <span>Make item "Power surged"</span>
+                <span>Make item &quot;Power surged&qout;</span>
             </React.Fragment>;
         }
 
