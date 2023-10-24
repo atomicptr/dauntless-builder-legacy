3863e1c01ae671e6e2f4af90c40244d1539d2ba7
Christopher Kaster <me@atomicptr.de>
2023-10-25 07:07:41 +0900

    update x.com url

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const issuesUrl = "https://github.com/atomicptr/dauntless-builder/issues";
-export const xTwitterUrl = "https://x.com/atomicptr";
+export const xTwitterUrl = "https://x.com/dauntlessbuildr";
 export const githubUrl = "https://github.com/atomicptr/dauntless-builder";
 export const discordServerUrl = "https://discord.gg/hkMvhsfPjH";
 export const crowdinLink = "https://crowdin.com/project/dauntless-builder";
