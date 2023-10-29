da51232c68baac7ac453888729f5395f34c4274b
Christopher Kaster <me@atomicptr.de>
2023-10-29 18:09:53 +0900

    add limited support for old data imports

--- src/utils/state.ts
+++ src/utils/state.ts
@@ -16,6 +16,18 @@ export const exportState = () => {
 
 export const persistState = (state: object) => {
     for (const [key, value] of Object.entries(state)) {
+        const isOldStateObject = ["favorites", "configuration"].indexOf(key) > -1;
+        const isStateObject = key.indexOf(stateIdent("")) === 0 || isOldStateObject;
+
+        if (!isStateObject) {
+            continue;
+        }
+
+        if (isOldStateObject) {
+            localStorage.setItem(stateIdent(key), JSON.stringify(value));
+            continue;
+        }
+
         localStorage.setItem(key, JSON.stringify(value));
     }
 };
