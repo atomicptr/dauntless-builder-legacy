7a9f9a230727716ea53c94129e5cbcf3f3e0f3f0
Christopher Kaster <me@atomicptr.de>
2024-03-03 13:09:44 +0100

    apply build time to enable cache purging

--- src/utils/asset-url.ts
+++ src/utils/asset-url.ts
@@ -1,3 +1,3 @@
 import { assetsCdnPrefix } from "@src/constants";
 
-export const assetUrl = (path: string) => assetsCdnPrefix + path;
+export const assetUrl = (path: string) => `${assetsCdnPrefix}${path.startsWith("/") ? path : "/" + path}?build=${DB_BUILD_TIME}`;
